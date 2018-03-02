a8c328b6762c2d1f44173bc41768cc5297ca1821
IvyMic <ibrahim_e@hotmail.co.uk>
2018-03-02 15:04:44 +0000

    Added many-to-many associations and created through table

---
+++ db/migrate/20180302143855_create_event_attendees.rb
@@ -0,0 +1,9 @@
+class CreateEventAttendees < ActiveRecord::Migration[5.1]
+  def change
+    create_table :event_attendees, :id => false do |t|
+      t.integer :event_attendee_id
+      t.integer :attendee_event_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228131552) do
+ActiveRecord::Schema.define(version: 20180302143855) do
+
+  create_table "event_attendees", id: false, force: :cascade do |t|
+    t.integer "event_attendee_id"
+    t.integer "attendee_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
