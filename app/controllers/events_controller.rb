6ab1be1b94d13a180e17ace5ede14aa20494b0a3
IvyMic <ibrahim_e@hotmail.co.uk>
2018-03-02 12:24:21 +0000

    Added events association with user and eventscontroller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name)
+    end
+end
